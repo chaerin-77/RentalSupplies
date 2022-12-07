c371452b67e5039564c3962143a5d73720c58be9
Jinyoung <74577889+jinyoung-2@users.noreply.github.com>
2022-12-08 00:46:29 +0900

    feat: 로그인, 로그아웃 상태에 따른 콘텐츠 변경  (#32)
    * feat: 로그인, 로그아웃 상태에 따라 콘텐츠 변경
    
    * fix: version으로 인한 오류로, 다른 방식으로 링크 연결

--- php/login.php
+++ php/login.php
@@ -4,12 +4,10 @@ session_start();
 
 include('db.php');
 
-$_SESSION['isSucessLogIn'] = false;
-
 if (isset($_POST['signIn'])){
 
   // 보안을 더욱 강화 (시큐어코딩, 보안코딩)
-  $role = mysqli_real_escape_string($db, $_POST['role']);
+  $role = $_POST['role'];
   $studentID = mysqli_real_escape_string($db, $_POST['studentID']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
@@ -18,13 +16,13 @@ if (isset($_POST['signIn'])){
     echo 
         "<script>
             window.alert('반드시 학번을 입력해주세요');
-            location.replace('../layout/singIn_Up.html');
+            location.replace('../layout/singIn_Up.php');
         </script>";
   }else if(empty($password)){
     echo 
         "<script>
             window.alert('반드시 비밀번호를 입력해주세요');
-            location.replace('../layout/singIn_Up.html');
+            location.replace('../layout/singIn_Up.php');
         </script>";
   }else{ 
       
@@ -40,13 +38,11 @@ if (isset($_POST['signIn'])){
     
     $result = mysqli_query($db, $sql);
     if(mysqli_num_rows($result) === 1){
-      // 1. 해당 열을 가져온다.
-      // 2. 가져올 때 배열의 형태로 가져온다. 
-      // 3. print_r()은 배열을 출력해주는 함수이다.
-      // 4. echo 쪼개서 가져올 수 있다. 
+
       $row = mysqli_fetch_assoc($result);  // 결과값 저장
       $hash = $row['Password']; // DB안에 있는 암호화된 값을 가져 옴
 
+      $_SESSION['isSuccessLogin'] = false;
       // password와 hash를 매핑시킴
       if(password_verify($password, $hash)){
         if(($_SESSION['isManager'] != false)){
@@ -55,18 +51,18 @@ if (isset($_POST['signIn'])){
           $_SESSION['studentID'] = $row['MID'];
         }
         $_SESSION['name'] = $row['Name'];
-        $_SESSION['isSucessLogIn'] = true;
 
+        $_SESSION['isSuccessLogin'] = true;
         echo 
           "<script>
               window.alert('로그인에 성공했습니다');
-              location.replace('../layout/main.html');
+              location.replace('../layout/main.php');
           </script>";
       }else{
         echo 
           "<script>
               window.alert('비밀번호가 잘못 입력되었습니다');
-              location.replace('../layout/main.html');
+              location.replace('../layout/main.php');
           </script>";
       }
 
@@ -74,7 +70,7 @@ if (isset($_POST['signIn'])){
       echo 
           "<script>
               window.alert('아이디가 잘못 입력되었습니다');
-              location.replace('../layout/main.html');
+              location.replace('../layout/main.php');
           </script>";
     }
   }    
@@ -83,7 +79,7 @@ if (isset($_POST['signIn'])){
   echo 
       "<script>
           window.alert('알 수 없는 오류가 발생했습니다');
-          location.replace('../layout/singIn_Up.html');
+          location.replace('../layout/singIn_Up.php');
       </script>";
 }
 ?>
